5111439d5905b89d2efb2ebaa876c886c9d38e0b
snows_l <snows_l@163.com>
2024-08-14 13:27:19 +0800

    perf

--- src/components/CustomImg/index.vue
+++ src/components/CustomImg/index.vue
@@ -3,20 +3,20 @@
  * @Author: snows_l snows_l@163.com
  * @Date: 2024-08-14 12:23:38
  * @LastEditors: snows_l snows_l@163.com
- * @LastEditTime: 2024-08-14 13:22:16
+ * @LastEditTime: 2024-08-14 13:26:17
  * @FilePath: /blog/src/components/CustomImg/index.vue
 -->
 <template>
   <el-image class="pointer el-img-warp" preview-teleported :src="src" :fit="fit" :previewSrcList="[src]" :lazy="true">
     <template #placeholder>
-      <div class="img-loading img-warp">
+      <div class="img-loading-warp img-warp">
         <img class="img-loading" src="@/assets/images/common/loading.svg" alt="loading" />
         <span class="img-loading-text text">{{ loadingText }}</span>
       </div>
     </template>
 
     <template #error>
-      <div class="img-error img-warp">
+      <div class="img-error-warp img-warp">
         <img class="img-error" src="@/assets/images/common/error.png" alt="error" />
         <span class="img-error-text text">😭 加载失败了</span>
       </div>
