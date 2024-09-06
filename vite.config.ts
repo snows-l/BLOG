b3210a86f62ab8997b2bfa6024c67b7568ba1378
snows_l <snows_l@163.com>
2024-09-06 23:00:09 +0800

    feat：开启gzip压缩

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,32 @@
  * @Author: snows_l snows_l@163.com
  * @Date: 2024-08-05 12:46:00
  * @LastEditors: snows_l snows_l@163.com
- * @LastEditTime: 2024-09-04 21:48:54
+ * @LastEditTime: 2024-09-06 22:51:19
  * @FilePath: /BLOG/vite.config.ts
  */
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import ElementPlus from 'unplugin-element-plus/vite'; // 引入ElementPlus插件
 import { defineConfig, loadEnv } from 'vite';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
   const config = loadEnv(mode, process.cwd());
   return {
-    plugins: [vue(), ElementPlus()],
+    plugins: [
+      vue(),
+      ElementPlus(),
+      viteCompression({
+        verbose: true, // 默认即可
+        disable: false, //开启压缩(不禁用)，默认即可
+        deleteOriginFile: false, //删除源文件
+        filter: /\.(js|mjs|json|css|html|ttf)$/i,
+        threshold: 10240, //压缩前最小文件大小
+        algorithm: 'gzip', //压缩算法
+        ext: '.gz' //文件类型
+      })
+    ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
