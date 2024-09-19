62585686f573a7219a9485170923098652bcb72c
snows_l <snows_l@163.com>
2024-09-19 22:39:14 +0800

    perf

--- src/components/Notice/index.vue
+++ src/components/Notice/index.vue
@@ -3,13 +3,14 @@
  * @Author: snows_l snows_l@163.com
  * @Date: 2024-09-19 15:01:50
  * @LastEditors: snows_l snows_l@163.com
- * @LastEditTime: 2024-09-19 15:23:08
- * @FilePath: /blog/src/components/Notice/index.vue
+ * @LastEditTime: 2024-09-19 22:38:52
+ * @FilePath: /BLOG/src/components/Notice/index.vue
 -->
 <template>
   <div class="notice-container-warp">
     <h2 class="title">NOTICE</h2>
     <div class="notice-content-padding">
+      <div class="sub-title">公告</div>
       <div class="notice-content">
         <div class="scrolling-text">
           小伙伴们大家好，我是snows_l，欢迎访问我的博客，可以互换友联，或者收藏博客。感兴趣的小伙伴可以
@@ -24,7 +25,6 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive, watch, onMounted, computed } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
@@ -44,9 +44,15 @@ const handleTo = (path: string) => {
   }
   .notice-content-padding {
     border-radius: 10px;
-    padding: 20px;
+    padding: 0 10px;
     background-color: var(--bg-content-color);
     box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
+    display: flex;
+    align-items: center;
+    .sub-title {
+      text-wrap: nowrap;
+      padding-right: 10px;
+    }
     .notice-content {
       overflow: hidden;
       padding: 20px;
